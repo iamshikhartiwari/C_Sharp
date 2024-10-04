deb9de937a1ff249e2cc53d443883740d50e9a3f
Shikhar Tiwari <72856503+iamshikhartiwari@users.noreply.github.com>
2024-10-04 12:47:25 +0530

    Update Customers.cs

--- ElectronicGadgets.Models/Customers.cs
+++ ElectronicGadgets.Models/Customers.cs
@@ -1,35 +1,3 @@
-﻿// namespace ElectronicGadgets.Models{
-//     public class Customer
-//     {
-//         public int CustomerID { get; set; }
-//         public string FirstName { get; set; }
-//         public string LastName { get; set; }
-//         public string Email { get; set; }
-//         public string Phone { get; set; }
-//         public string Address { get; set; }
-
-//         public int CalculateTotalOrders()
-//         {
-//             return 0; 
-//         }
-
-//         public void GetCustomerDetails()
-//         {
-//             Console.WriteLine($"Customer ID: {CustomerID}");
-//             Console.WriteLine($"Name: {FirstName} {LastName}");
-//             Console.WriteLine($"Email: {Email}");
-//             Console.WriteLine($"Phone: {Phone}");
-//             Console.WriteLine($"Address: {Address}");
-//         }
-
-//         public void UpdateCustomerInfo(string email, string phone, string address)
-//         {
-//             Email = email;
-//             Phone = phone;
-//             Address = address;
-//         }
-//     }
-// };
 
 
 namespace ElectronicGadgets.Models
